7215fc15bbb996c868b581c616d1f44aba22d14d
johnkiguma <154498415+johnkiguma@users.noreply.github.com>
2024-02-09 14:39:25 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
-
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import React, {useState} from "react";
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
 
 function App() {
+  const [stateVar, setStateVar] =useState(false);
+  const handleClick =()=>{
+    setStateVar((stateVar)=> !stateVar);
+  }
+
+  const appClass = stateVar ? "App dark" : "App Light"
+  const buttonText = stateVar ? "Dark Mode " : "Light Mode"
+  console.log(appClass)
+
   return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+    <div className="App light">
+    <div className={appClass}>
+      <header>
+        <h2>Shopster</h2>
+      <button onClick = {handleClick}> {buttonText}</button>
+       </header>
+      <ShoppingList items={itemData} />
+    </div>
     </div>
   );
 }
